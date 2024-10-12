6c10d832cf7204ab7ef7a083989138ccfa400b1a
Daniel Fahey <daniel.fahey@pm.me>
2024-10-12 19:38:10 +0100

    cuda-maintainers.cachix.org

--- nixos/ogma/configuration.nix
+++ nixos/ogma/configuration.nix
@@ -54,6 +54,8 @@
     "root"
     "daniel"
   ];
+  nix.settings.substituters = [ "https://cuda-maintainers.cachix.org" ];
+  nix.settings.trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
