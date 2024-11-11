495e871329cd8d13ba70fb8641c2c7b3582edcba
Daniel Fahey <daniel.fahey@pm.me>
2024-11-11 16:50:00 +0000

    add nextcloud health app

--- nixos/ogma/modules/nextcloud.nix
+++ nixos/ogma/modules/nextcloud.nix
@@ -23,6 +23,11 @@
       # registration # disabled
       # missing from pkgs.nextcloud30, but available in pkgs.nextcloud29:
       maps forms;
+      health = pkgs.fetchNextcloudApp {
+        url = "https://github.com/nextcloud/health/releases/download/v2.2.2/health.tar.gz";
+        hash = "sha256-JsmflNU/XIa46NJwdJ5xzjrBk3gI7mTthyqNJ5jhO1g=";
+        license = "agpl3Only";
+      };
     };
     extraAppsEnable = true;
     configureRedis = true;
