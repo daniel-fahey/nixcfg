3dc89f6bcb6f3231c7f817fb1dc319a796703d0b
Daniel Fahey <daniel.fahey@pm.me>
2024-12-12 21:08:45 +0000

    nextcloud upgrade

--- nixos/ogma/modules/nextcloud.nix
+++ nixos/ogma/modules/nextcloud.nix
@@ -9,7 +9,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud30;
     hostName = "cloud.${secrets.ogma.domain}";
     https = true;
     database.createLocally = true;
@@ -20,14 +20,9 @@
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps)
       memories cospend deck calendar contacts tasks notes polls spreed
-      # registration # disabled
-      # missing from pkgs.nextcloud30, but available in pkgs.nextcloud29:
+      registration # N.B. enabled
+      # recently available in pkgs.nextcloud30:
       maps forms;
-      health = pkgs.fetchNextcloudApp {
-        url = "https://github.com/nextcloud/health/releases/download/v2.2.2/health.tar.gz";
-        hash = "sha256-JsmflNU/XIa46NJwdJ5xzjrBk3gI7mTthyqNJ5jhO1g=";
-        license = "agpl3Only";
-      };
     };
     extraAppsEnable = true;
     configureRedis = true;
